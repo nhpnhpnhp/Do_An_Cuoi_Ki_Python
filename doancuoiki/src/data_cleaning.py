741b946b22b065b8ec41cf8987e8b3eb361319d8
Phat.Nguyen <97361367+nhpnhpnhp@users.noreply.github.com>
2024-11-29 22:37:11 +0700

    Update data_cleaning.py

--- doancuoiki/src/data_cleaning.py
+++ doancuoiki/src/data_cleaning.py
@@ -1,29 +1,29 @@
 import pandas as pd
-from data_loading import load_data
 
+# hàm đưa giá trị lỗi về biên 0 hoặc 500
+def fix_out_of_bounds(value, min_val=0, max_val=500):
+    if value < min_val:
+        return min_val  
+    elif value > max_val:
+        return max_val 
+    return value
 def clean_data(df):
     # Lọc các giá trị lỗi kiểu dữ liệu giá trị ở các trường về NaN
     columns_to_convert = ["AQI Value", "CO AQI Value", "Ozone AQI Value", "NO2 AQI Value", "PM2.5 AQI Value"]
     for column in columns_to_convert:
         df[column] = pd.to_numeric(df[column], errors='coerce', downcast="integer")
 
-    # Xóa các bản ghi với trường dữ liệu AQI Value không hợp lệ
-    df_cleaned = df[(df['AQI Value'].between(0, 500)) &
-                     (df["CO AQI Value"].between(0, 500)) &
-                     (df['Ozone AQI Value'].between(0, 500)) &
-                     (df["NO2 AQI Value"].between(0, 500)) &
-                     (df["PM2.5 AQI Value"].between(0, 500))]
-
+    #Đưa các giá trị nằm ngoài khoảng [0;500] về giá trị biên
+    for col in columns_to_convert:
+        df[col] = df[col].apply(fix_out_of_bounds)
+        
     # Xóa các bản ghi trùng lặp
-    df_cleaned = df_cleaned.drop_duplicates()
-
-    # Xóa toàn bộ các bản ghi với trường dữ liệu trống hoặc NaN
+    df_cleaned = df.drop_duplicates()
+    
+    # Xóa các bản ghi có cột dữ liệu trống hoặc NaN
     df_cleaned = df_cleaned.dropna()
 
     print("Đã xóa", len(df) - len(df_cleaned), "bản ghi lỗi")
+        
     return df_cleaned
-
-if __name__ == "__main__":
-    df = load_data(r"C:\Users\THINKPAD\doancuoiki\data\raw\global_air_pollution_dataset.csv")
-    if df is not None:
-        cleaned_df = clean_data(df)
+    
